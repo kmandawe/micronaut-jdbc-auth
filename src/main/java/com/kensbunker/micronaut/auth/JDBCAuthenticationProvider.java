f0e4295fb0cff582972ecd97be48daf58e8ddf06
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-11-16 14:50:47 +0800

    Custom JWT

--- src/main/java/com/kensbunker/micronaut/auth/JDBCAuthenticationProvider.java
+++ src/main/java/com/kensbunker/micronaut/auth/JDBCAuthenticationProvider.java
@@ -1,6 +1,5 @@
 package com.kensbunker.micronaut.auth;
 
-
 import com.kensbunker.micronaut.auth.persistence.UserEntity;
 import com.kensbunker.micronaut.auth.persistence.UserRepository;
 import io.micronaut.http.HttpRequest;
@@ -12,7 +11,8 @@ import io.micronaut.security.authentication.AuthenticationResponse;
 import io.micronaut.security.authentication.UserDetails;
 import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
-import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Optional;
 import javax.annotation.Nullable;
 import javax.inject.Singleton;
@@ -31,27 +31,34 @@ public class JDBCAuthenticationProvider implements AuthenticationProvider {
   }
 
   @Override
-  public Publisher<AuthenticationResponse> authenticate(@Nullable HttpRequest<?> httpRequest,
-      AuthenticationRequest<?, ?> authenticationRequest) {
+  public Publisher<AuthenticationResponse> authenticate(
+      @Nullable HttpRequest<?> httpRequest, AuthenticationRequest<?, ?> authenticationRequest) {
     final String identity = (String) authenticationRequest.getIdentity();
     LOG.debug("User {} tries to login...", identity);
-    return Flowable.create(emitter -> {
-      Optional<UserEntity> maybeUser = users.findByEmail(identity);
-      if (maybeUser.isPresent()) {
-      LOG.debug("Found user: {}", maybeUser.get().getEmail());
-        String secret = (String) authenticationRequest.getSecret();
-        if (maybeUser.get().getPassword().equals(secret)) {
-          // pass
-          LOG.debug("User logged in.");
-          emitter.onNext(new UserDetails(identity, new ArrayList<>()));
-          return;
-        } else {
-          LOG.debug("Wrong password provided for user {}", identity);
-        }
-      } else {
-        LOG.debug("No user found with email: {}", identity);
-      }
-      emitter.onError(new AuthenticationException(new AuthenticationFailed("Wrong username or password")));
-    }, BackpressureStrategy.ERROR);
+    return Flowable.create(
+        emitter -> {
+          Optional<UserEntity> maybeUser = users.findByEmail(identity);
+          if (maybeUser.isPresent()) {
+            LOG.debug("Found user: {}", maybeUser.get().getEmail());
+            String secret = (String) authenticationRequest.getSecret();
+            if (maybeUser.get().getPassword().equals(secret)) {
+              // pass
+              LOG.debug("User logged in.");
+              final HashMap<String, Object> attributes = new HashMap<>();
+              attributes.put("hair_color", "brown");
+              attributes.put("language", "en");
+              emitter.onNext(
+                  new UserDetails(identity, Collections.singletonList("ROLE_USER"), attributes));
+              return;
+            } else {
+              LOG.debug("Wrong password provided for user {}", identity);
+            }
+          } else {
+            LOG.debug("No user found with email: {}", identity);
+          }
+          emitter.onError(
+              new AuthenticationException(new AuthenticationFailed("Wrong username or password")));
+        },
+        BackpressureStrategy.ERROR);
   }
 }
--- src/main/java/com/kensbunker/micronaut/auth/SecuredEndpoint.java
+++ src/main/java/com/kensbunker/micronaut/auth/SecuredEndpoint.java
@@ -3,14 +3,27 @@ package com.kensbunker.micronaut.auth;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.authentication.Authentication;
 import io.micronaut.security.rules.SecurityRule;
+import java.security.Principal;
+import java.util.Arrays;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Secured(SecurityRule.IS_AUTHENTICATED)
 @Controller("/secured")
 public class SecuredEndpoint {
 
+  private static final Logger LOG = LoggerFactory.getLogger(SecuredEndpoint.class);
+
   @Get("/status")
-  public String status() {
-    return "You are authenticated!";
+  public List<Object> status(Principal principal) {
+    Authentication details = (Authentication) principal;
+    LOG.debug("User Details: {}", details.getAttributes());
+    return Arrays.asList(
+        details.getName(),
+        details.getAttributes().get("hair_color"),
+        details.getAttributes().get("language"));
   }
 }
